030b79537cd1b339e36b021a02141774013000be
larcondo <larcondo90@gmail.com>
2023-08-23 11:07:00 -0300

    add express-async-errors package and delete try/catch

--- controllers/notes.js
+++ controllers/notes.js
@@ -7,15 +7,11 @@ notesRouter.get('/', async (request, response) => {
 })
 
 notesRouter.get('/:id', async (request, response, next) => {
-  try {
-    const note = await Note.findById(request.params.id)
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  } catch(exeption) {
-    next(exeption)
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
   }
 })
 
@@ -27,22 +23,14 @@ notesRouter.post('/', async (request, response, next) => {
     important: body.important || false,
   })
 
-  try {
-    const savedNote = await note.save()
-    response.status(201).json(savedNote)
-  } catch(exeption) {
-    next(exeption)
-  }
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 
 })
 
 notesRouter.delete('/:id', async (request, response, next) => {
-  try {
     await Note.findByIdAndRemove(request.params.id)
     response.status(204).end()
-  } catch(exeption) {
-    next(exeption)
-  }
 })
 
 notesRouter.put('/:id', (request, response, next) => {
